b18e7fa7d8c5ec225912b83e0fafa1b6c6a94ce9
cmaynard <chrissy.swe.maynard@gmail.com>
2023-06-23 14:46:11 -0400

    add changes to resolver

--- src/main/java/com/emr/graphql/component/resolvers/FakeAlternativeMedicineDataResolver.java
+++ src/main/java/com/emr/graphql/component/resolvers/FakeAlternativeMedicineDataResolver.java
@@ -2,6 +2,7 @@ package com.emr.graphql.component.resolvers;
 
 import com.netflix.dgs.codegen.generated.types.AlternativeMedicine;
 import com.netflix.dgs.codegen.generated.types.AlternativeMedicineFilter;
+import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 import org.apache.commons.lang3.StringUtils;
@@ -13,29 +14,29 @@ import java.util.stream.Collectors;
 
 import static com.emr.graphql.datasource.fake.FakeAlternativeMedicineDAO.ALTERNATIVE_MEDICINE_LIST;
 
+@DgsComponent
 @Configuration
 public class FakeAlternativeMedicineDataResolver {
 
     @DgsQuery(field = "alternativeMedicine")
             public List<AlternativeMedicine> getAlternativeMedicine(@InputArgument(name = "alternativeMedicineFilter",
     collectionType = AlternativeMedicineFilter.class) Optional<AlternativeMedicineFilter> filter) {
-        if(filter.isEmpty()){
-            return ALTERNATIVE_MEDICINE_LIST;
-        }
+        return filter.map(alternativeMedicineFilter -> ALTERNATIVE_MEDICINE_LIST.stream().filter(
+                alternativeMedicine -> this.matchAlternativeMedicineFilter(
+                        alternativeMedicineFilter, alternativeMedicine)
+        ).collect(Collectors.toList())).orElse(ALTERNATIVE_MEDICINE_LIST);
 
-       return ALTERNATIVE_MEDICINE_LIST.stream().filter(
-               alternativeMedicine -> this.matchAlternativeMedicineFilter(
-                       filter.get(), alternativeMedicine)
-       ).collect(Collectors.toList());
     }
 
     private boolean matchAlternativeMedicineFilter(AlternativeMedicineFilter filter, AlternativeMedicine alternativeMedicine){
-        if(StringUtils.isNotBlank(filter.getService().toString())
+        if(StringUtils.isNotBlank(filter.getService())
             && alternativeMedicine.getService().contains(
-                    filter.getService().toString().toLowerCase())){
+                    filter.getService().toLowerCase())){
                         return false;
         }
 
+        // TODO - add validation for patient MRN as well
+        // realistically patients are verified via DOB, name and MRN
         if(filter.getPatient() != null
                 && !StringUtils.containsIgnoreCase(filter.getPatient().getName(),
                 StringUtils.defaultIfBlank(filter.getPatient().getName(), StringUtils.EMPTY)
